113666d8ebadf05e4da96a72916113414e4df0c3
Boots2291 <buteramv@gmail.com>
2017-09-06 15:25:23 -0600

    Add authentication

--- spec/features/guest_creates_a_user_account_spec.rb
+++ spec/features/guest_creates_a_user_account_spec.rb
@@ -20,22 +20,22 @@ RSpec.describe "guest becomes a user" do
       expect(page).to have_content("Hi, mb")
       expect(page).to have_link("Logout")
 
-      # click_on "Logout"
-      #
-      # expect(current_path).to eq(root_path)
-      #
-      # expect(page).to have_link("Already a user? Click Here")
-      #
-      # click_on "Already a user? Click Here"
-      #
-      # expect(current_path).to eq(login_path)
-      #
-      # fill_in "session[username]", with: "mb"
-      # fill_in "session[password]", with: "1234"
-      #
-      # click_on "Login"
-      #
-      # expect(page).to have_content("Hi, mb")
+      click_on "Logout"
+
+      expect(current_path).to eq(root_path)
+
+      expect(page).to have_link("Login")
+
+      click_on "Login"
+
+      expect(current_path).to eq(login_path)
+
+      fill_in "session[username]", with: "mb"
+      fill_in "session[password]", with: "1234"
+
+      click_on "Login"
+
+      expect(page).to have_content("Hi, mb")
     end
   end
 end
